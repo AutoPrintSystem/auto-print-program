df79fba4cca63f3a0b36c643cdfd621aea9f5c0a
JangSoyun <soyoon5328@gmail.com>
2023-08-24 09:30:26 +0900

    feat : 파일 조회 table 생성

--- auto-print-scheduler/src/screens/ControlScreen.java
+++ auto-print-scheduler/src/screens/ControlScreen.java
@@ -1,10 +1,46 @@
 package screens;
 
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.Collections;
+import java.util.Vector;
 
 public class ControlScreen extends JPanel {
+    FileTable fileTable;
     public ControlScreen(){
         JLabel label = new JLabel("컨트롤화면입니다.");
         this.add(label);
+
+        fileTable = new FileTable();
+        JScrollPane scrollPane = new JScrollPane(fileTable);
+
+        this.add(scrollPane);
     }
+
+    public class FileTable extends JTable{
+        private DefaultTableModel tableModel;
+        private String[] headers={"번호","파일명","컬러/흑백","출력시간", "상태"};
+        public FileTable(){
+            Vector<String> header = createHeader(headers);
+
+
+            tableModel = new DefaultTableModel(headers, 5) {
+                @Override
+                public boolean isCellEditable(int row, int column) {
+                    return false; // Make all cells not editable
+                }
+            };
+            this.setModel(tableModel);
+
+
+        }
+        public Vector<String> createHeader(String[] inputList) { // create Table Header
+            Vector<String> outputList = new Vector<String>();
+            Collections.addAll(outputList, inputList);
+            return outputList;
+        }
+    }
+
 }
